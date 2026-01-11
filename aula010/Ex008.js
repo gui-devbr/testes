1f1ae49bf39e52f1f54743aae28515908ac1316b
gui-devbr <adm.gui.network@gmail.com>
2026-01-11 09:29:40 -0300

    atualizando

---
+++ aula010/Ex008.js
@@ -0,0 +1,7 @@
+var vel = 50.2
+console.log(`A velocidade e ${vel} km/hr` )
+
+if (vel > 60){ //condiçao simples
+console.log(`Voce ultrapassou a velocidade permetida. MULTADO!`)}
+
+console.log("dirija sempre usando cinto de segurança!")
